c5d7525acfd582ad16b311dab98c0707b31e8390
Sandy Pham <SandyPham092595@gmail.com>
2024-12-30 23:25:48 -0600

    Prework

---
+++ Python Fundamentals.py
@@ -0,0 +1,24 @@
+# Python Fundamentals
+
+# 1. Hello World Program: Write and run a simple program that prints Hello, World! (Chapter 2).
+print("Hello, World!")
+
+# 2. Working with Lists: Create a list of your favorite hobbies and print each one using a for loop.
+hobbies = ["Reading", "Cooking", "Drawing", "Gaming", "Fashion"]
+print("My favorite hobbies are:")
+
+# Modify the list by adding, removing, and sorting the items using list methods (e.g., append(), pop(), sort().
+
+# Adding a hobby.
+hobbies.append("Traveling")
+print(hobbies)
+
+# Removing a hobby.
+hobbies.pop(4)
+print(hobbies)
+
+# Sorting the list.
+hobbies.sort()
+print(hobbies)
+
+# https://github.com/SandyPham092595/Python-Fundamentals.git
